bdb458427f3c83de6dde693884df2580c9a8554a
Woody Sims <sims.woodrow@gmail.com>
2022-10-14 15:36:49 -0500

    acquire modifications

--- acquire.py
+++ acquire.py
@@ -6,7 +6,7 @@ from env import get_db_url
 from os import path
 
 def get_df_from_sql_cached(query,database,filename=''):
-    if filename is '':
+    if filename == '':
         filename = 'data/' + database + '.csv'
     if path.isfile(filename):
         return pd.read_csv(filename)
@@ -19,7 +19,7 @@ def get_titanic_data():
     query = '''
     SELECT * FROM passengers
     '''
-    return get_df_from_sql_cached(query, 'titanic')
+    return get_df_from_sql_cached(query, 'titanic_db','titanic')
 
 
 def get_iris_data():
@@ -27,15 +27,17 @@ def get_iris_data():
     SELECT * FROM measurements
     JOIN species USING(species_id)
     '''
-    return get_df_from_sql_cached(query,'iris')
+    return get_df_from_sql_cached(query,'iris_db','iris')
 
 def get_telco_data():
     query = '''
-    SELECT * FROM employees
+    SELECT * FROM customers
     JOIN contract_types USING(contract_type_id)
     JOIN internet_service_types USING(internet_service_type_id)
     JOIN payment_types USING(payment_type_id)
     '''
     return get_df_from_sql_cached(query, 'telco_churn')
-
-
+if __name__ == '__main__':
+    print(get_titanic_data().head())
+    print(get_iris_data().head())
+    print(get_telco_data().head())
