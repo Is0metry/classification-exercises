506f86a5c35383c8cdcb88f2767a2d8bcb3a5d91
Woody Sims <sims.woodrow@gmail.com>
2022-10-14 15:02:28 -0500

    doing the classification exercises correctly now

--- acquire.py
+++ acquire.py
@@ -1,46 +1,41 @@
 from genericpath import isfile
+from webbrowser import get
 import numpy as np
 import pandas as pd
 from env import get_db_url
-import os
+from os import path
+
+def get_df_from_sql_cached(query,database,filename=''):
+    if filename is '':
+        filename = 'data/' + database + '.csv'
+    if path.isfile(filename):
+        return pd.read_csv(filename)
+    url = get_db_url(database)
+    df = pd.read_sql(query, url)
+    df.to_csv(database + '.csv')
+    return df
 
 def get_titanic_data():
-    filename = 'titanic.csv'
-    if os.isfile(filename):
-        return pd.read_csv(filename)
-    url = get_db_url('titanic_db')
     query = '''
     SELECT * FROM passengers
     '''
-    titanic_df = pd.read_sql(query,url)
-    titanic_df.to_csv(filename)
-    return titanic_df
+    return get_df_from_sql_cached(query, 'titanic')
 
 
 def get_iris_data():
-    filename = 'iris.csv'
-    if os.isfile(filename):
-        return pd.read_csv(filename)
     query = '''
     SELECT * FROM measurements
     JOIN species USING(species_id)
     '''
-    url = get_db_url('iris')
-    iris_df = pd.read_sql(query,url)
-    iris_df.to_csv(filename)
-    return iris_df
+    return get_df_from_sql_cached(query,'iris')
 
 def get_telco_data():
-    filename = 'telco.csv'
-    if os.isfile(filename):
-        return pd.read_csv(filename)
-    url = get_db_url('telco_churn')
     query = '''
     SELECT * FROM employees
     JOIN contract_types USING(contract_type_id)
     JOIN internet_service_types USING(internet_service_type_id)
     JOIN payment_types USING(payment_type_id)
     '''
-    df = pd.read_csv(query,url)
-    df.to_csv(filename)
-    return df
+    return get_df_from_sql_cached(query, 'telco_churn')
+
+
